23971468a90f3b1783e2a343bfea71b38d5eaa48
Michael Cullen <michaelcullen2011@hotmail.co.uk>
2022-03-28 11:14:01 +0100

    changed cv image, updated cv information to be more deloitte relevant, added qc paper and a link to the paper

---
+++ .history/mysite/urls_20220328105901.py
@@ -0,0 +1,29 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from pages.views import homepage_view, contact_view, cv_view, success_view, tableau1_view, tableau2_view
+
+urlpatterns = [
+    path('', homepage_view),
+    path('home/', homepage_view),
+    path('contact/', contact_view, name='contact'),
+    path('contact/success/', success_view, name='success'),
+    path('cv/', cv_view),
+    path('qc_neutrino_paper/', cv_view),
+    path('tableau1/', tableau1_view),
+    path('tableau2/', tableau2_view),
+]
